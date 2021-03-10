882001c23a7cfc51391aaa7dac62c1436acfa7f9
Usman Ahmad Saeed <20966983+UsmanAhmadSaeed@users.noreply.github.com>
2021-03-10 08:54:12 +0000

    logged json response

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ var hashh = window.location.hash.substr(1)
 
 if (window.location.hash != "") {
     $.getJSON(endpoint + "/" + hashh, function (data) {
+        console.log(endpoint+"/"+hashh)
         data = data["result"];
 
         if (data != null) {
